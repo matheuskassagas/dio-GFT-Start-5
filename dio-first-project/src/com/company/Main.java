559971d0600a057f87234dc3da53705eadcc84e1
Matheus Gigliotti <gigliotti_@hotmail.com>
2022-05-09 15:05:59 -0300

    Second commit

--- dio-first-project/src/com/company/Main.java
+++ dio-first-project/src/com/company/Main.java
@@ -2,6 +2,8 @@ package com.company;
 
 import com.company.entity.Gato;
 
+import java.util.Objects;
+
 public class Main {
 
         public static void main(String[] args) {
@@ -13,5 +15,61 @@ public class Main {
             Gato cat1 = new Gato();
 
             System.out.println(cat1);
+
+            Livro livro1 = new Livro("Livro sem nome", 1000);
+
+            System.out.println(livro1);
+    }
+    
+}
+
+class Livro {
+    public String name;
+    public Integer numberPage;
+
+    public Livro() {
+    }
+
+    public Livro(String name, Integer numberPage) {
+        this.name = name;
+        this.numberPage = numberPage;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getNumberPage() {
+        return numberPage;
+    }
+
+    public void setNumberPage(Integer numberPage) {
+        this.numberPage = numberPage;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Livro livro = (Livro) o;
+        return Objects.equals(name, livro.name) && Objects.equals(numberPage, livro.numberPage);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, numberPage);
+    }
+
+    @Override
+    public String toString() {
+        final StringBuilder sb = new StringBuilder("Livro{");
+        sb.append("name='").append(name).append('\'');
+        sb.append(", numberPage=").append(numberPage);
+        sb.append('}');
+        return sb.toString();
     }
 }
